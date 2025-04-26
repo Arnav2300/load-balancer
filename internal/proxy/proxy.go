03339fdf80ac623269d037ed923970c897294bff
Arnav2300 <arnav.sar@gmail.com>
2025-04-26 20:41:10 +0000

    rev proxy set

---
+++ internal/proxy/proxy.go
@@ -0,0 +1,24 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func NewProxy(target *url.URL) *httputil.ReverseProxy {
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	return proxy
+}
+func ProxyRequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Host = url.Host
+		r.URL.Scheme = url.Scheme
+		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+		r.Host = url.Host
+		path := r.URL.Path
+		r.URL.Path = strings.TrimLeft(path, endpoint)
+		proxy.ServeHTTP(w, r)
+	}
+}
